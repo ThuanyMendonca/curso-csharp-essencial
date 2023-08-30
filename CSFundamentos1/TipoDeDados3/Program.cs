1a3233f2d5843c3c57cbe1c9c08ed03ad11de6ae
Thuany Mendonca <thuany1997@hotmail.com>
2023-08-30 00:23:45 -0300

    First commit

---
+++ CSFundamentos1/TipoDeDados3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Atribuindo valores a boolean e char");
+
+bool ativo = true;
+bool inativo = false;
+
+
+Console.WriteLine(ativo);
+Console.WriteLine(inativo);
+
+Console.WriteLine(10 == 15);
+Console.WriteLine(10 == 10);
+
+int x = 10;
+int y = 15;
+
+Console.WriteLine(x > y);
+
+char letra1 = 'A';
+
+Console.WriteLine(letra1);
+
+char letra2 = '\u0041';
+Console.WriteLine(letra2);
+
+
+Console.ReadLine();
---
+++ CSFundamentos1/TipoDeDados4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Definindo variáveis do tipo string, object e dynamic");
+
+string nome = "Curso C#";
+String titulo = "Curso C# Essencial";
+
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+string valor = "isso é uma string";
+
+valor = "isso é uma string com valor alterado";
+
+Console.WriteLine(valor);
+
+Console.ReadLine();
