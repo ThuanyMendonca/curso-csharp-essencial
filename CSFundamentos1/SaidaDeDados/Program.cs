3567bc467431c7804081be603abfa4aa3ffbd1ec
Thuany Mendonca <thuany1997@hotmail.com>
2023-09-16 15:53:03 -0300

    Saida de dados e escape

---
+++ CSFundamentos1/SaidaDeDados/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Saída de dados: Usando sequencia de escapes");
+
+string local = "c:\\dados\\poesias.txt";
+string frase = "Ele falou: \"Não fui eu\"";
+
+Console.WriteLine(local);
+Console.WriteLine(frase);
+
+Console.ReadLine();
