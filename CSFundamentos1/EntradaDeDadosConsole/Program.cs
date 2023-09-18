2bb5a5f8fc677b67d7c77757d9842660b1eb5805
Thuany Mendonca <thuany1997@hotmail.com>
2023-09-18 11:28:45 -0300

    Entrada de dados

---
+++ CSFundamentos1/EntradaDeDadosConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Entrada de dados");
+
+Console.WriteLine("\nInforme o seu nome:");
+
+string? nome = Console.ReadLine();
+
+Console.WriteLine($"O seu nome é {nome}");
+
+
+// utilizando entrada de dados int
+Console.WriteLine("Informe a sua idade:");
+int idade = Console.Read();
+
+Console.WriteLine($"A sua idade é: {idade}");
+
+Console.ReadKey();
